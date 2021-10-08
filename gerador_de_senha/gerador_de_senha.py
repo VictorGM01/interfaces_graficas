e7dfbcf8193ad949ff955e22493acd2b84fcd0dd
VictorGM01 <victormarques8801@gmail.com>
2021-10-08 19:44:52 -0300

    adiciona comentários

--- gerador_de_senha/gerador_de_senha.py
+++ gerador_de_senha/gerador_de_senha.py
@@ -17,7 +17,7 @@ class GeradorDeSenha:
              sg.Checkbox('', default=False, key='senha intermediaria')],
             [sg.Text('Senha Forte', size=(19, 1)),
              sg.Checkbox('', default=False, key='senha forte')],
-            [sg.Spin([x for x in range(1, 30)], initial_value=1, key='chars'),
+            [sg.Spin([x for x in range(1, 60)], initial_value=1, key='chars'),
              sg.Text('Quantidade de Caracteres')],
             [sg.Output(size=(40, 3))],
             [sg.Button('', button_color=('#F0F0F0', '#F0F0F0'),
@@ -32,25 +32,41 @@ class GeradorDeSenha:
                                 element_justification='c')
 
     def iniciar(self):
+        # verifica os eventos que estão acontecendo (Ex.: clique nos botões)
         while True:
             evento, valores = self.janela.read()
             if evento == sg.WINDOW_CLOSED or evento == 'x':
-                break
+                break  # interrompe o código caso o usuário feche a janela
+
+            # acontece caso o botão de gerar senha seja pressionado
             elif evento == 'Gerar Senha':
+
+                # executa quando o checkbox "senha fraca" é marcado
                 if valores['senha fraca']:
+
+                    # gera senha com caracteres minúsculos
                     senha_fraca = ''.join(random.choice(string.ascii_lowercase)
                                           for x in range(
                         int(valores['chars']))
                                           )
                     print(senha_fraca)
+
+                # executa quando o checkbox "senha intermediária" é marcado
                 if valores['senha intermediaria']:
+
+                    # gera senha com caracteres minúsculos e maiúsculos
                     senha_inter = ''.join(random.choice(string.ascii_letters +
                                                         string.digits)
                                           for x in range(
                         int(valores['chars']))
                                           )
                     print(senha_inter)
+
+                # executa quando o checkbox "senha forte" é marcado
                 if valores['senha forte']:
+
+                    # gera senha com caracteres minúsculos e maiúsculos,
+                    # dígitos e caracteres especiais
                     senha_forte = ''.join(random.choice(string.ascii_letters +
                                                         string.digits +
                                                         "@#!&$#%*/ç.;:?¨()" +
