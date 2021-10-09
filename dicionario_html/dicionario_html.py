90c970f57e8ee1e231a9ba2ddd96143de29abb9c
VictorGM01 <victormarques8801@gmail.com>
2021-10-09 13:20:10 -0300

    armazena icons em pasta diferente

--- dicionario_html/dicionario_html.py
+++ dicionario_html/dicionario_html.py
@@ -7,7 +7,7 @@ def janela_inicial() -> sg.Window:
     layout = [
         [sg.Text('Dicionário de Tags do HTML',
                  font=('Helvetica', 25), justification='c'),
-         sg.Image(filename='html.png')],
+         sg.Image(filename='icons\html.png')],
         [sg.Text('Adicionar tag ao dicionário', size=(30, 1)),
          sg.Checkbox('', default=False, key='add')],
         [sg.Text('Procurar tag específica', size=(30, 1)),
@@ -17,8 +17,8 @@ def janela_inicial() -> sg.Window:
         [sg.Text('Ler todo o dicionário', size=(30, 1)),
          sg.Checkbox('', default=False, key='ler dict')],
         [sg.Button('', image_size=(24, 24),
-                   image_filename='ok.png', key='ok'),
-         sg.Button('', image_filename='clear.png', key='x')]
+                   image_filename='icons\ok.png', key='ok'),
+         sg.Button('', image_filename='icons\clear.png', key='x')]
     ]
     # janela
     return sg.Window('Dicionário HTML', layout,
@@ -31,14 +31,14 @@ def janela_adiciona_tag() -> sg.Window:
     layout = [
         [sg.Text('Adicionar uma Nova Tag ao Dicionário',
                  font=('Helvetica', 15), justification='c'),
-         sg.Image(filename='icon_add_html.png')],
+         sg.Image(filename='icons\icon_add_html.png')],
         [sg.Text('Nome da Tag', size=(15, 1)),
          sg.Input(key='nome da tag', size=(30, 1))],
         [sg.Text('Função da Tag', size=(15, 1)),
          sg.Input(key='função da tag', size=(30, 1))],
         [sg.Output(size=(50, 1))],
-        [sg.Button('', image_filename='add.png', key='adicionar tag'),
-         sg.Button('', image_filename='clear.png', key='cancelar adição')]
+        [sg.Button('', image_filename=r'icons\add.png', key='adicionar tag'),
+         sg.Button('', image_filename='icons\clear.png', key='cancelar adição')]
     ]
     # janela
     return sg.Window('Adicionar Tag', layout,
@@ -51,13 +51,13 @@ def janela_busca_tag() -> sg.Window:
     layout = [
         [sg.Text('Pesquisar uma Tag Específica', font=('Helvetica', 15),
                  justification='c'),
-         sg.Image(filename='icon_search.png')],
+         sg.Image(filename='icons\icon_search.png')],
         [sg.Text('Nome da Tag', size=(15, 1)),
          sg.Input(key='nome da tag - busca', size=(30, 1))],
         [sg.Text('Resultado:', size=(10, 1))],
         [sg.Output(size=(50, 2))],
-        [sg.Button('', image_filename='icon_btn_busca.png', key='buscar tag'),
-         sg.Button('', image_filename='clear.png', key='cancelar busca')]
+        [sg.Button('', image_filename='icons\icon_btn_busca.png', key='buscar tag'),
+         sg.Button('', image_filename='icons\clear.png', key='cancelar busca')]
     ]
     # janela
     return sg.Window('Buscar Tag', layout, element_justification='c')
@@ -69,14 +69,14 @@ def janela_busca_funcao():
     layout = [
         [sg.Text('Pesquisar Função Específica', font=('Helvetica', 15),
                  justification='c'),
-         sg.Image(filename='icon_busca_func.png')],
+         sg.Image(filename='icons\icon_busca_func.png')],
         [sg.Text('Função', size=(10, 1)),
          sg.Input(key='função da tag', size=(30, 1))],
         [sg.Text('Resultado(s):', size=(10, 1))],
         [sg.Output(size=(50, 2))],
-        [sg.Button('', image_filename='icon_btn_busca.png',
+        [sg.Button('', image_filename='icons\icon_btn_busca.png',
                    key='buscar função'),
-         sg.Button('', image_filename='clear.png', key='cancelar pesquisa')]
+         sg.Button('', image_filename='icons\clear.png', key='cancelar pesquisa')]
     ]
     # janela
     return sg.Window('Buscar Função', layout, element_justification='c')
