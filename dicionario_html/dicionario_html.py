49e53c1928f4fb8001991a227f5043bd16ac99ba
VictorGM01 <victormarques8801@gmail.com>
2021-10-08 22:36:27 -0300

    personaliza primeira janela

--- dicionario_html/dicionario_html.py
+++ dicionario_html/dicionario_html.py
@@ -6,7 +6,8 @@ def janela_inicial():
     sg.theme('DarkPurple1')
     layout = [
         [sg.Text('Dicionário de Tags do HTML',
-                 font=('Helvetica', 25), justification='c')],
+                 font=('Helvetica', 25), justification='c'),
+         sg.Image(filename='html.png')],
         [sg.Text('Adicionar tag ao dicionário', size=(30, 1)),
          sg.Checkbox('', default=False, key='add')],
         [sg.Text('Procurar tag específica', size=(30, 1)),
@@ -15,7 +16,18 @@ def janela_inicial():
          sg.Checkbox('', default=False, key='buscar funcao')],
         [sg.Text('Ler todo o dicionário', size=(30, 1)),
          sg.Checkbox('', default=False, key='ler dict')],
-        [sg.Button('', button_color=('#F0F0F0', '#F0F0F0'),
-                   image_filename='circulo.png')]
-
+        [sg.Button('', image_size=(24, 24),
+                   image_filename='ok.png', key='ok'),
+         sg.Button('', image_filename='clear.png', key='x')]
     ]
+    # janela
+    return sg.Window('Dicionário HTML', layout,
+                     element_justification='center')
+
+
+janela = janela_inicial()
+
+while True:
+    evento, valores = janela.read()
+    if evento == sg.WINDOW_CLOSED or evento == 'x':
+        break
